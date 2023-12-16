2c907164466e78270fd09f67faf6f50b23741c9c
Felipe Moreira <ti.felipemoreira@gmail.com>
2023-12-16 15:52:29 -0300

    ajustes LF

--- docker/dockerfiles/build/appserver/entrypoint-appserver.sh
+++ docker/dockerfiles/build/appserver/entrypoint-appserver.sh
@@ -1,3 +1,3 @@
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:.
-mv /tmp/appserver.ini /totvs/protheus12/binarios/appserver/appserver.ini
+cp /tmp/appserver.ini /totvs/protheus12/binarios/appserver/appserver.ini
 cd /totvs/protheus12/binarios/appserver && chmod 777 appsrvlinux && ./appsrvlinux
